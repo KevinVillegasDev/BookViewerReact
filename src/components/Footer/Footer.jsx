d77c2d298b79b14a288976093abfcf2f6efb87f3
KevinVillegasDev <kevin.villegas94@gmail.com>
2021-08-11 08:20:24 -0700

    Created footer component and styled it

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css'
+
+
+const Footer = () => {
+    return (
+                <div id="footer">
+                    2021, dCC Library
+                </div>
+
+     );
+}
+ 
+export default Footer;
